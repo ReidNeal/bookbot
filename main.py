749a2416c00a5520792e02107bb03146183f3040
ReidNeal <reidneal2@gmail.com>
2025-03-02 11:20:48 -0500

    add character_count function

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count(book_text)
+    char_count_dict = character_count(book_text)
+    print(char_count_dict)
 
 def get_book_text(book_path):
     with open(book_path)as f:
         return f.read()
 
-from stats import word_count
+from stats import *
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,25 @@
-
+''' this function takes in a string from and returns the number of words in the string
+    Parameters: get_book_text - string
+    Returns: word_count - integer
+'''
 def word_count(get_book_text):
     word_count = 0
     for word in get_book_text.split():
         word_count += 1
     return print(f"{word_count} words found in the document")
+
+''' this function takes in a string and returns a dictionary of the number of times each character appears in the string
+    Parameters: get_book_text - string
+    Returns: char_count_dict - dictionary
+'''
+def character_count(get_book_text):
+    char_count_dict = {}
+    lower_characters = get_book_text.lower()
+    for char in lower_characters:
+        if char.isalpha():
+            if char in char_count_dict:
+                char_count_dict[char] += 1
+            else:
+                char_count_dict[char] = 1
+    return char_count_dict
+
